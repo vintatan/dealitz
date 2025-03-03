19b3665eeaaef247f78c998169dbdad1c17123cf
Vincent Tatan <tatanvincent13@gmail.com>
2025-03-04 01:08:18 +0800

    [Fix] Add unused import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import Image from "next/image"
-import { ArrowRight } from "lucide-react"
+import { ArrowRight, CheckCircle } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card"
